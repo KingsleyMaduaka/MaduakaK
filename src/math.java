13277c808f47609a1f7a341a37b94081dc701222
Deniel Alekseev <daniel.neo.eu@icloud.com>
2019-12-03 15:24:17 +0100

    Changed sub method by m22

--- src/math.java
+++ src/math.java
@@ -4,6 +4,9 @@ class Math{
         return first + second;
     }
     public int sub(int first, int second){
+	System.out.println("Sub method");
+	System.out.println("Sub method");
+	System.out.println("Sub method");	
         return first - second;
     }
 }
